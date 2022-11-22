cd5640d57d726d2b904e49e0f2eb4e6628a3f595
João Matheus S. Silva <jtheo6@hotmail.com>
2022-11-21 23:13:45 -0300

    adicionando cálculo do FW
    TODO: Imprimir a matriz de menor caminho antes do FW e depois do FW

--- src/service/FloydWarshal.java
+++ src/service/FloydWarshal.java
@@ -3,6 +3,7 @@ package service;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Edge;
 import model.Graph;
 import model.Vertex;
 
@@ -21,6 +22,26 @@ public class FloydWarshal {
       minDistances[i][i] = 0;
     }
 
+    for (Vertex vertex : listVertex) {
+      for (Edge edge : graph.getVertices().get(vertex)) {
+
+        System.out.println(vertex.getLabel() + " - " + listVertex.indexOf(vertex));
+
+        System.out.println(vertex + " " + edge.toString());
+
+        minDistances[listVertex.indexOf(vertex)][listVertex.indexOf(edge.getVertex())] = edge.getWeight();
+      }
+    }
+
+    for (int k = 0; k < listVertex.size(); k++) {
+      for (int i = 0; i < listVertex.size(); i++) {
+        for (int j = 0; j < listVertex.size(); j++) {
+          if (minDistances[i][k] + minDistances[k][j] < minDistances[i][j]) {
+            minDistances[i][j] = minDistances[i][k] + minDistances[k][j];
+          }
+        }
+      }
+    }
   }
 
 }
